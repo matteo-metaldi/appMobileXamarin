3796a6d04c565801ed2cceafd4519c2aaa1ca7fc
matteo.metaldi <matteo.metaldi@student.supsi.ch>
2022-04-08 10:18:09 +0200

    added skeleton

---
+++ MeteoApp/ViewModels/MeteoItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace MeteoApp
+{
+    public class MeteoItemViewModel : BaseViewModel
+    {
+        Entry _entry;
+
+        public Entry Entry
+        {
+            get { return _entry;  }
+            set
+            {
+                _entry = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MeteoItemViewModel(Entry entry)
+        {
+            Entry = entry;
+        }
+    }
+}
---
+++ MeteoApp/Views/MeteoListPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MeteoApp
+{
+    public partial class MeteoListPage : ContentPage
+    {
+        public MeteoListPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MeteoListViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        void OnItemAdded(object sender, EventArgs e)
+        {
+            _ = DisplayAlert("Messaggio", "Testo", "OK");
+        }
+
+        void OnListItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                Navigation.PushAsync(new MeteoItemPage()
+                {
+                    BindingContext = new MeteoItemViewModel(e.SelectedItem as Entry)
+                });
+            }
+        }
+    }
+}
