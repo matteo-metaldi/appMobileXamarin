580b16659d878a4afbfa80a72533105dedebb860
matteo.metaldi <matteo.metaldi@student.supsi.ch>
2022-05-03 14:45:55 +0200

    deleted comments

--- MeteoApp/TestDatabase.cs
+++ MeteoApp/TestDatabase.cs
@@ -13,37 +13,22 @@ namespace MeteoApp
 
         public TestDatabase()
         {
-            // collegamento al database
             var dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "MeteoApp.db");
             database = new SQLiteAsyncConnection(dbPath);
 
-            // crea la tabella se non esiste
             database.CreateTableAsync<Location>().Wait();
         }
-
-        /*
-         * Ritorna una lista con tutti gli items.
-         */
         public Task<List<Location>> GetItemsAsync()
         {
             return database.Table<Location>().ToListAsync();
         }
-
-
-        /*
-         * Salvataggio o update.
-         */
         public Task<int> SaveItemAsync(Location item)
         {
-            if (item.ID == 0) // esempio
+            if (item.ID == 0) 
                 return database.UpdateAsync(item);
 
             return database.InsertAsync(item);
         }
-
-        /*
-         * Cancellazione di un elemento.
-         */
         public Task<int> DeleteItemAsync(Location item)
         {
             return database.DeleteAsync(item);
